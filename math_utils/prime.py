169d7492ddbb9c1371627111042a5043cb0f7b70
your user name <your user email>
2022-01-26 22:08:07 +0000

    initial implementation of isprime function.

--- math_utils/prime.py
+++ math_utils/prime.py
@@ -1,8 +1,10 @@
 from math import sqrt
 def isprime(n):
-    p=0
-    "check whether n is a prime number"
-    for i in range(2,int(sqrt(n))+1):
-        if (n%i==0):
-            return False
-    return True
+    if n==1:
+        return False
+    else:
+        "check whether n is a prime number"
+        for i in range(2,int(sqrt(n))+1):
+            if (n%i==0):
+                return False
+        return True
